c66220bbd08da67b1764aa72db366df804736e77
JennyGD1 <jenniferlima61@gmail.com>
2025-11-10 12:59:45 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 3000;
 
 // Configurações
-const GAS_TOKEN_URL = "https://script.google.com/macros/s/AKfycbypQ1Smx0v-2w4brX8FV3D52op3RvKsfzyxoHNq05Fm5AdGDAHaYqvhN7lQ2VY4Ir-H/exec";
+const GAS_TOKEN_URL = process.env.GAS_TOKEN_URL;
 const BASE_URL = "https://df-regulacao-api-live.gdf.live.maida.health";
 const EVENTOS_GUIA_URL = "https://df-eventos-guia-api-live.gdf.live.maida.health";
 
